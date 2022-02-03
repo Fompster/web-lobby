b15fef18bc88eea22a37bd5b406248bbcb93c5fb
Fompster <fompsta@gmail.com>
2022-02-03 15:37:59 +0100

    .

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {  res.send('<h1>Hello world</h1>');});
-server.listen(3000, () => {  console.log('listening on *:3000');});
+app.get('/', (req, res) => {  res.send('./index.html');});
+io.on('connection', (socket) => {  console.log('a user connected');});
+server.listen(5501, () => {  console.log('listening on *:5501');});
